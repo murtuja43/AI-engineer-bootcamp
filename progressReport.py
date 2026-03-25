648aa1164ba33e40ffdfbd8af6618db9bbe7bea5
Md. Golam Murtuja Kayes <murtujakayes99@gmail.com>
2026-03-25 23:54:10 +0500

    new progress

--- progressReport.py
+++ progressReport.py
@@ -6,4 +6,5 @@
 # practiced day 2's tasks one more day to remember more accurately
 # practiced day 2's tasks one more day to remember more accurately
 # practiced day 2's tasks one more day to remember more accurately
+# practiced day 2's tasks one more day to remember more accurately
 # Started doing day 3 (learned to read csv, write/save csv)
